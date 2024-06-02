312e053e68f021f1a778bf21f5e1e89f1683a673
kaikdorvalo <kaikdorvalo@gmail.com>
2024-06-02 17:12:48 -0300

    adicionado class validator

--- src/dtos/user/create-user.dto.ts
+++ src/dtos/user/create-user.dto.ts
@@ -1,23 +1,48 @@
-export class CreateUserDTO {
-    firstName: string
-    lastName: string
-    cpf: string
-    phoneNumber: string
-    date: string
-    email: string
-    password: string
-    state: string
-    city: string
+import { IsDateString, IsEmail, IsString, Length, MinLength } from "class-validator"
 
-    constructor(firstName: string, lastName: string, cpf: string, phoneNumber: string, date: string, email: string, password: string, state: string, city: string) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.cpf = cpf;
-        this.phoneNumber = phoneNumber;
-        this.date = date;
-        this.email = email;
-        this.password = password;
-        this.state = state;
-        this.city = city;
+export class CreateUserDTO {
+
+    @IsString()
+    @MinLength(3)
+    firstName: string;
+
+    @MinLength(3)
+    @IsString()
+    lastName: string;
+
+    @IsString()
+    @Length(14, 14)
+    cpf: string;
+
+    @IsString()
+    @Length(16, 16)
+    phoneNumber: string;
+
+    @IsDateString()
+    date: string;
+
+    @IsEmail()
+    email: string;
+
+    @IsString()
+    @MinLength(10)
+    password: string;
+
+    @IsString()
+    state: string;
+
+    @IsString()
+    city: string;
+
+    constructor(data: { firstName: string, lastName: string, cpf: string, phoneNumber: string, date: string, email: string, password: string, state: string, city: string }) {
+        this.firstName = data.firstName;
+        this.lastName = data.lastName;
+        this.cpf = data.cpf;
+        this.phoneNumber = data.phoneNumber;
+        this.date = data.date;
+        this.email = data.email;
+        this.password = data.password;
+        this.state = data.state;
+        this.city = data.city;
     }
 }
--- src/model/user/user.controller.ts
+++ src/model/user/user.controller.ts
@@ -1,11 +1,22 @@
 import { Request, Response } from 'express'
 import userService from './user.service'
+import { CreateUserDTO } from '../../dtos/user/create-user.dto';
+import { HttpStatus } from '../../enums/http-status.enum';
+import { validate } from 'class-validator';
 
 class UserController {
 
     async create(req: Request, res: Response) {
-        const response = await userService.create(req.body)
-        return res.status(response.statusCode).send(response.metaData)
+        const newUser = new CreateUserDTO(req.body);
+        return validate(newUser).then(async (errors) => {
+            if (errors.length > 0) {
+                console.log(errors)
+                return res.status(HttpStatus.BAD_REQUEST).send('Bad Request');
+            } else {
+                const response = await userService.create(new CreateUserDTO(req.body));
+                return res.status(response.statusCode).send(response.metaData)
+            }
+        })
     }
 
     async findById(req: Request, res: Response) {
