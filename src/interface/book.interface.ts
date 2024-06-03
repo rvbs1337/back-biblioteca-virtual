fb78de91522efea1d3dd04125b5bb877206c4e17
Rubens <rubenspereira9@hotmail.com>
2024-06-02 21:26:41 -0300

    essentials book

---
+++ src/interface/book.interface.ts
@@ -0,0 +1,11 @@
+export interface Book {
+    _id: string,
+    title: string,
+    author: string,
+    condition: string,
+    image: string,
+    date: Date,
+    email: string,
+    createdAt: Date,
+    updatedAt: Date
+}
---
+++ src/model/book/book.schema.ts
@@ -0,0 +1,35 @@
+import { Document,Schema,model } from "mongoose";
+import { Book } from "src/interface/book.interface";
+
+export type BookDocument = Book & Document;
+
+const bookSchema =  new Schema({
+    title:{
+        type: String,
+        required: [true, 'title']
+    },
+    author:{
+        type: String,
+        required: [true, 'author']
+    },
+    condition:{
+        type: String,
+        required: [true, 'condition']
+    },
+    image:{
+        type: String,
+        required: [true, 'image']
+    },
+    date:{
+        type: Date,
+        required: [true, 'date']
+    },
+    email:{
+        type: String,
+        required: [true, 'email']
+    },
+},{
+    timestamps: true,
+})
+
+export default model<BookDocument>("Book", bookSchema)
